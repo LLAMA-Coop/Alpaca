39bec476e504fae17243a96629941ebd69f040c6
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-06-25 14:55:56 -0400

    Set up API folder with route folders and the start of route handlers

---
+++ app/api/note/route.js
@@ -0,0 +1,8 @@
+export async function GET(req) {
+    return new Response(
+      JSON.stringify({
+        message: "You have successfully received a response from /api/note",
+      })
+    );
+  }
+  
