435f463248975196ff6cba13dd8b27693a9388bd
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-07-11 20:24:34 -0400

    Style SourceDisplay component

--- app/components/sourceDisplay.js
+++ app/components/sourceDisplay.js
@@ -1,26 +1,27 @@
 import Link from "next/link";
+import styles from "./sourceDisplay.module.css";
 
 export default function SourceDisplay({ source }) {
   return (
-    <>
+    <div className={styles.source}>
       <h4>{source.title}</h4>
       <p>
         <em>Medium: </em>
         {source.medium}
       </p>
-      <p>
-        <em>Contributors: </em>
-      </p>
-      {source.contributors.length > 0 ? (
-        <ol>
-          {source.contributors.map((cont) => {
-            return <li key={cont}>{cont}</li>;
-          })}
-        </ol>
-      ) : (
-        <p>No contributors listed</p>
-      )}
+      <div className={styles.contributors}>
+        <p>Contributors:</p>
+        {source.contributors.length > 0 ? (
+          <ol>
+            {source.contributors.map((cont) => {
+              return <li key={cont}>{cont}</li>;
+            })}
+          </ol>
+        ) : (
+          <p>No contributors listed</p>
+        )}
+      </div>
       <Link href={source.url}>Click here to visit source page</Link>
-    </>
+    </div>
   );
 }
