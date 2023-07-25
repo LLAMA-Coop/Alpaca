ab67156eac58cd59dc51300e4174b044c48a4acc
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-07-25 10:35:22 -0400

    Begin work on QuizDisplay

--- app/components/quiz/quizDisplay.js
+++ app/components/quiz/quizDisplay.js
@@ -1,4 +1,7 @@
-import styles from './quizDisplay.module.css'
+"use client";
+import makeUniqueId from "@/app/code/uniqueId";
+import { useEffect, useState } from "react";
+import styles from "./quizDisplay.module.css";
 
 // What the quiz displays depends on whether it is a client-checked or server-checked quiz component
 // If client-checked, everything is rendered from the Quiz object
@@ -7,3 +10,28 @@ import styles from './quizDisplay.module.css'
 // Server-checked is for testing apps that want to prevent cheating
 
 // sent into component via canClientCheck boolean
+
+export default function Quiz({ canClientCheck, quiz }) {
+  let [userResponse, setUserResponse] = useState("");
+
+  let [uniqueId, setUniqueId] = useState("");
+  useEffect(() => {
+    setUniqueId(makeUniqueId());
+  }, []);
+
+  return (
+    <div className={styles.quiz}>
+      <p>{quiz.prompt}</p>
+      <label>
+        Your Response
+        <input
+          type="text"
+          defaultValue={userResponse}
+          onChange={(e) => {
+            setUserResponse(e.target.value);
+          }}
+        ></input>
+      </label>
+    </div>
+  );
+}
