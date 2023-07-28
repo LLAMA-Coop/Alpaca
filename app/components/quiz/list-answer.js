b2d54586fe090b216974fb0ba2575a9eced4ffd5
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-07-27 21:24:04 -0400

    Add ListAnswer component

---
+++ app/components/quiz/list-answer.js
@@ -0,0 +1,73 @@
+"use client";
+import { useState } from "react";
+
+export default function ListAnswer({ canClientCheck, quiz, isOrdered }) {
+  let [userResponse, setUserResponse] = useState(
+    [...Array(quiz.correctResponses.length)].map(() => "")
+  );
+  let [responseStatus, setResponseStatus] = useState("empty");
+  let [responseCorrect, setResponseCorrect] = useState(false);
+
+  function handleChange(index, value) {
+    setResponseStatus("incomplete");
+    let array = [...userResponse];
+    array[index] = value;
+    setUserResponse(array);
+  }
+
+  function handleCheckAnswer() {
+    setResponseStatus("complete");
+    if (isOrdered) {
+      isIncorrect = userResponse.find((res, index) => {
+        return res.toLowerCase() !== quiz.correctResponses[index].toLowerCase();
+      });
+      if(isIncorrect == undefined){
+        setResponseCorrect(true);
+      }
+    }
+
+    if(!isOrdered){
+        let userAnswers = Set(userResponse);
+        let correctAnswers = Set(quiz.correctAnswers);
+        if(userAnswers.length !== correctAnswers.length){
+            setResponseCorrect(false);
+            return;
+        }
+        userAnswers.forEach((ans) => {
+            // Finish this
+        })
+    }
+  }
+
+  return (
+    <div>
+      <p id="prompt">{quiz.prompt}</p>
+      <ul>
+        {quiz.correctResponses.map((ans, index) => {
+          <li key={index}>
+            <input
+              type="text"
+              aria-labelledby="prompt"
+              id={"ans_" + index}
+              onChange={(e) => handleChange(index, e.target.value)}
+            ></input>
+          </li>;
+        })}
+      </ul>
+
+      <button onClick={handleCheckAnswer}>Check Answer</button>
+
+      {responseCorrect && responseStatus === "complete" && <div>Correct!</div>}
+      {!responseCorrect && responseStatus === "complete" && (
+        <div>
+          Incorrect. Acceptable answers are
+          <ul>
+            {quiz.correctResponses.map((ans) => {
+              return <li key={ans}>{ans}</li>;
+            })}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
