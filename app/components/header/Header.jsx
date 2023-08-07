a6acc7d08d4783865e73f03e3073fd2aedf6a058
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-08-07 13:10:37 -0400

    Correct 'non-plain object' error by serializing user object from useUser that was being passed into Profile component

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -3,9 +3,10 @@ import { DynamicNav } from "./DynamicNav";
 import styles from "./Header.module.css";
 import { useUser } from "@/lib/auth";
 import Link from "next/link";
+import { serializeOne } from "@/lib/db";
 
 export async function Header() {
-    const user = await useUser();
+    const user = serializeOne(await useUser());
 
     return (
         <header className={styles.header}>
