a92811cb171663c87afd061f5db4fb63f88d8a25
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-08-24 15:57:14 -0400

    Make a border with colors chasing each other Only for when hovering
    This was initiated by LIBRO, though he was unable to make a pull request through GitHub
    I did some tweaks to make it on hover and improve styling and consistency

--- app/components/card/Card.jsx
+++ app/components/card/Card.jsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useRouter } from "next/navigation";
 import styles from "./Card.module.css";
 
-export function Card({
+export const Card = ({
     title,
     subtitle,
     description,
@@ -15,112 +13,100 @@ export function Card({
     buttons,
     children,
     border,
-}) {
-    const router = useRouter();
-
+}) => {
     return (
         <div className={styles.container}>
-            <div
-                style={{
-                    background:
-                        border === "green"
-                            ? `linear-gradient(
-                                var(--gradient-angle),
-                                var(--accent-tertiary-outline),
-                                hsla(0, 0%, 100%, 0.1),
-                                var(--accent-tertiary-1),
-                                hsla(0, 0%, 100%, 0.3)
-                            )`
-                            : border === "red"
-                            ? `linear-gradient(
-                                var(--gradient-angle),
-                                var(--accent-secondary-outline),
-                                hsla(0, 0%, 100%, 0.1),
-                                var(--accent-secondary-1),
-                                hsla(0, 0%, 100%, 0.3)
-                            )`
-                            : "",
-                }}
-            />
+            <span className={styles.span}></span>
 
-            <div
-                tabIndex={url ? 0 : -1}
-                className={styles.content}
-                onClick={() => url && router.push(url)}
-                onKeyDown={(e) => e.key === "Enter" && url && router.push(url)}
-                style={{ cursor: url ? "pointer" : "" }}
-            >
-                {url && (
-                    <div className={styles.urlIcon}>
-                        <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                    </div>
-                )}
+            {/* <div
+        style={{
+          background:
+            border === "green"
+              ? `linear-gradient(
+        var(--gradient-angle),
+        var(--accent-tertiary-outline),
+        hsla(0, 0%, 100%, 0.1),
+        var(--accent-tertiary-1),
+        hsla(0, 0%, 100%, 0.3)
+      )`
+              : border === "red"
+              ? `linear-gradient(
+        var(--gradient-angle),
+        var(--accent-secondary-outline),
+        hsla(0, 0%, 100%, 0.1),
+        var(--accent-secondary-1),
+        hsla(0, 0%, 100%, 0.3)
+      )`
+              : "",
+        }}
+      /> */}
 
-                {image && (
-                    <div>
-                        <img src={image} alt={title} />
-                    </div>
-                )}
+            <div className={styles.content_container}>
+                <div className={styles.content}>
+                    {image && (
+                        <div>
+                            <img src={image} alt={title} />
+                        </div>
+                    )}
 
-                {(title || subtitle || description) && (
-                    <div className={styles.textContent}>
-                        {(title || subtitle) && (
-                            <div>
-                                {title && (
-                                    <header title={title}>{title}</header>
-                                )}
-                                {subtitle && <h5>{subtitle}</h5>}
-                            </div>
-                        )}
-                        {description && <p>{description}</p>}
-                    </div>
-                )}
+                    {(title || subtitle || description) && (
+                        <div className={styles.textContent}>
+                            {(title || subtitle) && (
+                                <div>
+                                    {title && (
+                                        <header title={title}>{title}</header>
+                                    )}
+                                    {subtitle && <h5>{subtitle}</h5>}
+                                </div>
+                            )}
+                            {description && <p>{description}</p>}
+                        </div>
+                    )}
 
-                {children && (
                     <div className={styles.childrenContent}>{children}</div>
-                )}
 
-                {buttons?.length > 0 && (
-                    <div className={styles.buttonContainer}>
-                        {buttons.map((button) => {
-                            if (button.link) {
-                                return (
-                                    <a
-                                        key={button.label}
-                                        href={button.link}
-                                        target="_blank"
-                                        rel="noreferrer"
-                                        className={`button ${button.color}`}
-                                    >
-                                        {button.label}
+                    {buttons?.length > 0 && (
+                        <div className={styles.buttonContainer}>
+                            {buttons.map((button) => {
+                                if (button.link) {
+                                    return (
+                                        <a
+                                            key={button.label}
+                                            href={button.link}
+                                            target="_blank"
+                                            rel="noreferrer"
+                                            className={`button ${button.color}`}
+                                        >
+                                            {button.label}
 
-                                        {button.icon && (
-                                            <FontAwesomeIcon
-                                                icon={button.icon}
-                                            />
-                                        )}
-                                    </a>
-                                );
-                            } else {
-                                return (
-                                    <button
-                                        onClick={button.onClick}
-                                        className={`button ${button.color}`}
-                                        key={button.label}
-                                    >
-                                        {button.label}{" "}
-                                        {button.icon && (
-                                            <FontAwesomeIcon
-                                                icon={button.icon}
-                                            />
-                                        )}
-                                    </button>
-                                );
-                            }
-                        })}
-                    </div>
-                )}
+                                            {button.icon && (
+                                                <FontAwesomeIcon
+                                                    icon={button.icon}
+                                                />
+                                            )}
+                                        </a>
+                                    );
+                                } else {
+                                    return (
+                                        <button
+                                            onClick={button.onClick}
+                                            className={button.color}
+                                            key={button.label}
+                                        >
+                                            {button.label}{" "}
+                                            {button.icon && (
+                                                <FontAwesomeIcon
+                                                    icon={button.icon}
+                                                />
+                                            )}
+                                        </button>
+                                    );
+                                }
+                            })}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
-}
+};
