6a734b0ec3fc08f88b7d5cc430f45c33050d5588
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-09-09 21:05:10 -0400

    Add PUT route to api/note

--- app/api/note/route.js
+++ app/api/note/route.js
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
-import { queryReadableResources, useUser } from "@/lib/auth";
-import { Note } from "@mneme_app/database-models";
+import { canEdit, queryReadableResources, useUser } from "@/lib/auth";
+import { Note, Quiz } from "@mneme_app/database-models";
 import { server, unauthorized } from "@/lib/apiErrorResponses";
+import { Types } from "mongoose";
+import { serializeOne } from "@/lib/db";
 
 export async function GET(req) {
     try {
@@ -65,6 +67,67 @@ export async function POST(req) {
         return NextResponse.json({ content }, { status: 201 });
     } catch (error) {
         console.error(`[Note] POST error: ${error}`);
+        return server;
+    }
+}
+
+export async function PUT(req) {
+    try {
+        const user = await useUser();
+
+        if (!user) {
+            return unauthorized;
+        }
+
+        const { _id, text, sources, permissions } = await req.json();
+
+        const note = await Note.findById(_id);
+        if (!note) {
+            return NextResponse.json(
+                {
+                    message: `No note found with id ${_id}`,
+                },
+                { status: 404 },
+            );
+        }
+
+        if (!canEdit(note, user)) {
+            return NextResponse.json(
+                {
+                    message: `You are not permitted to edit note ${_id}`,
+                },
+                { status: 403 },
+            );
+        }
+
+        if (text) {
+            note.text = text;
+        }
+        if (sources) {
+            sources.forEach((sourceId_req) => {
+                if (
+                    !note.sources.find(
+                        (srcId) => srcId.toString() == sourceId_req,
+                    )
+                ) {
+                    note.sources.push(new Types.ObjectId(sourceId_req));
+                }
+            });
+        }
+
+        if (permissions && note.createdBy.toString() === user._id.toString()) {
+            note.permissions = serializeOne(permissions);
+        }
+
+        if (!note.contributors.includes(user._id)) {
+            note.contributors.push(user._id);
+        }
+        note.updateBy = user._id;
+
+        const content = await note.save();
+        return NextResponse.json({ content });
+    } catch (error) {
+        console.error(`[Note] PUT error: ${error}`);
         return server;
     }
 }
