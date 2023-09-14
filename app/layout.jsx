c5ef3fd3f599146cb0153d022e6952b324e13a44
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-09-13 22:41:49 -0400

    Forgot to let store have user._id

--- app/layout.jsx
+++ app/layout.jsx
@@ -29,6 +29,7 @@ export default async function RootLayout({ children }) {
             ? [...user.associates, ...publicUsers]
             : [...publicUsers],
     ).map(x => ({
+        _id: x._id,
         username: x.username,
         displayName: x.displayName,
         avatar: x.avatar,
