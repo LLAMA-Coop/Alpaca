f4d4d07f6f8f73304ea4a518aba29d1b6a3fa308
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-11-19 14:23:21 -0500

    Create Category model

---
+++ app/api/models/Category.js
@@ -0,0 +1,47 @@
+import MAX from "@/lib/max";
+import PermissionSchema from "./PermissionSchema";
+import { model, models, Schema } from "mongoose";
+
+const CategorySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: MAX.title,
+        },
+        subcategoryOf: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "category",
+            },
+        ],
+        prerequisites: [
+            {
+                category: {
+                    type: Schema.Types.ObjectId,
+                    ref: "category",
+                },
+                requiredAverageLevel: {
+                    type: Number,
+                    default: 1,
+                },
+            },
+        ],
+        permissions: PermissionSchema,
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: "user",
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+CategorySchema.set("toJSON", {
+    virtuals: true,
+});
+
+export default models?.category || model("category", CategorySchema);
