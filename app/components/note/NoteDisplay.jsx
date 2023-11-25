2fe2028b474ec4cd5dbf326635ef5ae630f5d2de
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-11-25 17:27:14 -0500

    Add courses to display components for note and source

--- app/components/note/NoteDisplay.jsx
+++ app/components/note/NoteDisplay.jsx
@@ -1,11 +1,11 @@
 import styles from "./NoteDisplay.module.css";
 import { Card, ListItem } from "@components/client";
 // import { Source, User } from "@mneme_app/database-models";
-import { Note, Source, User } from "@/app/api/models";
+import { Note, User } from "@/app/api/models";
 
 export async function NoteDisplay({ note }) {
     const user = await User.findById(note.createdBy);
-    const dbNote = await Note.findById(note._id).populate("sources");
+    const dbNote = await Note.findById(note._id).populate("sources").populate("courses");
 
     return (
         <Card description={`${note.text}`}>
@@ -22,20 +22,36 @@ export async function NoteDisplay({ note }) {
                 )}
             </div>
 
-            <div>Sources linked:</div>
-            <ul>
-                {dbNote.sources.map((source) => {
-                    return (
-                        <li key={source._id}>
-                            <a className={styles.sourceLink} href={source.url}>
-                                <div>{source.title}</div>
+            <div className={styles.sources}>
+                <h5>Sources linked</h5>
+                <ul>
+                    {dbNote.sources.map((source) => {
+                        return (
+                            <ListItem
+                                key={source._id}
+                                item={source.title}
+                                link={source.url}
+                            />
+                        );
+                    })}
+                </ul>
+            </div>
 
-                                <div>{source.medium}</div>
-                            </a>
-                        </li>
-                    );
-                })}
-            </ul>
+            <div className={styles.tags}>
+                <h5>This note belongs to the following courses</h5>
+                {dbNote.courses && dbNote.courses.length > 0 ? (
+                    <ul>
+                        {dbNote.courses.map((course) => {
+                            return (
+                                <ListItem key={course._id} item={course.name} />
+                            );
+                        })}
+                    </ul>
+                ) : (
+                    <p>No Courses Listed</p>
+                )}
+            </div>
+
             <p>Created by: {user?.username ?? "Unknown"}</p>
         </Card>
     );
