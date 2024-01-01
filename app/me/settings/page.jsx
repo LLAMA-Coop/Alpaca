3c1bd5bd520f1e3b32d0f56cc901588c3b39e9a3
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-12-31 19:28:48 -0500

    Create page, component, and API route handlers for casting votes

--- app/me/settings/page.jsx
+++ app/me/settings/page.jsx
@@ -1,8 +1,8 @@
 import { redirect } from "next/navigation";
 import { serializeOne } from "@/lib/db";
-import { cookies } from "next/headers";
 import { Settings } from "./Settings";
 import { useUser } from "@/lib/auth";
+import { cookies } from "next/headers";
 
 export default async function SettingsPage() {
     const user = await useUser({ token: cookies().get("token")?.value });
