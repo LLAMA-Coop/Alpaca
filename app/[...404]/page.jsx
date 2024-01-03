5ee1fe3acc8a49a5d53b4868986b27cb841daa31
DarthDippy <shpadoinkleday@gmail.com>
2024-01-03 11:02:37 -0500

    fix lang

--- app/[...404]/page.jsx
+++ app/[...404]/page.jsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-// Note this file and 404 folder is required only for development purposes. There is a NextJS bug that keeps reloading the 404 page only in
+// Note this file and 404 folder are required only for development purposes. There is a NextJS bug that keeps reloading the 404 page only in
 // development mode. It is not required in Production.
 // https://stackoverflow.com/questions/76585950/nextjs-not-found-page-keeps-reloading-every-3-seconds
 export default function NotFound() {
