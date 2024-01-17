b8c0d5a27e04333a7aada08b1e6f6212a942f6cc
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2024-01-16 20:15:13 -0500

    Update individual course page to be more like Dashboard

---
+++ app/courses/[id]/CourseDash.jsx
@@ -0,0 +1,404 @@
+"use client";
+
+import { useStore } from "@/store/store";
+import { useState } from "react";
+import styles from "@/app/me/dashboard/Dash.module.css";
+import {
+    Card,
+    QuizDisplay,
+    UserStats,
+    ListItem,
+} from "@/app/components/client";
+
+const tabs = [
+    {
+        name: "Course Info",
+        icon: (
+            <g>
+                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+            </g>
+        ),
+    },
+    {
+        name: "Quizzes",
+        icon: (
+            <g>
+                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+            </g>
+        ),
+    },
+    {
+        name: "Notes",
+        icon: (
+            <g>
+                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+            </g>
+        ),
+    },
+    {
+        name: "Sources",
+        icon: (
+            <g>
+                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+            </g>
+        ),
+    },
+];
+
+export function CourseDash({ course }) {
+    const user = useStore((state) => state.user);
+    const noteStore = useStore((state) => state.notes);
+    const sourceStore = useStore((state) => state.sources);
+    const quizStore = useStore((state) => state.quizzes);
+    const courses = useStore((state) => state.courses);
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [currentTab, setCurrentTab] = useState(
+        parseInt(localStorage ? localStorage.getItem("currentTab") || 0 : 0),
+    );
+
+    if (!user) return null;
+
+    function filter(x) {
+        return x.courses.find((c) => c.toString() === course._id);
+    }
+
+    const quizzes = quizStore.filter(filter);
+    const notes = noteStore.filter(filter);
+    const sources = sourceStore.filter(filter);
+
+    let sum = 0;
+    let countOfCanLevel = 0;
+    let lowestLevel;
+    const userQuizzes = quizzes.map((q) => {
+        const userQuiz = user.quizzes.find(
+            (x) => x.quizId.toString() === q._id.toString(),
+        );
+        if (userQuiz) {
+            sum += userQuiz.level;
+            lowestLevel =
+                lowestLevel == undefined || userQuiz.level < lowestLevel
+                    ? userQuiz.level
+                    : lowestLevel;
+            if (new Date(userQuiz.hiddenUntil) < Date.now()) {
+                countOfCanLevel++;
+            }
+            return userQuiz;
+        }
+        countOfCanLevel++;
+        return {
+            quizId: q._id,
+            lastCorrect: new Date(0),
+            level: 0,
+            hiddenUntil: new Date(0),
+        };
+    });
+    const averageLevel = sum / quizzes.length;
+
+    return (
+        <main className={styles.main}>
+            <section>
+                <nav>
+                    <ul className={styles.tabList}>
+                        {tabs.map((tab, index) => (
+                            <li
+                                key={tab.name}
+                                tabIndex={0}
+                                className={
+                                    currentTab === index ? styles.active : ""
+                                }
+                                onClick={() => {
+                                    setCurrentTab(index);
+                                    localStorage.setItem("currentTab", index);
+                                }}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                        setCurrentTab(index);
+                                        localStorage.setItem(
+                                            "currentTab",
+                                            index,
+                                        );
+                                    }
+                                }}
+                            >
+                                <div>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        {tab.icon}
+                                    </svg>
+                                </div>
+
+                                <span>{tab.name}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+
+                <div className={styles.content}>
+                    <h1>{course.name}</h1>
+                    <h2>{course.description}</h2>
+                    <header>
+                        <h3>
+                            {tabs[currentTab].name}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                            >
+                                {tabs[currentTab].icon}
+                            </svg>
+
+                            {currentTab === 1 && (
+                                <span>
+                                    Average Level: {averageLevel.toFixed(3)}
+                                </span>
+                            )}
+
+                            {currentTab === 1 && (
+                                <span>Lowest Level: {lowestLevel}</span>
+                            )}
+
+                            {currentTab === 1 && (
+                                <span>
+                                    Available to Level Up: {countOfCanLevel}
+                                </span>
+                            )}
+
+                            {currentTab === 2 && (
+                                <span>Available Notes: {notes.length}</span>
+                            )}
+
+                            {currentTab === 3 && (
+                                <span>Available Sources: {sources.length}</span>
+                            )}
+                        </h3>
+                    </header>
+
+                    <main className="scrollbar">
+                        <ul>
+                            {currentTab === 0 && (
+                                <Card
+                                    title={course.name}
+                                    description={course.description}
+                                >
+                                    <div className={styles.card}>
+                                        <h4>Parent Courses</h4>
+                                        {course.parentCourses &&
+                                        course.parentCourses.length ? (
+                                            <ol className="chipList">
+                                                {course.parentCourses.map(
+                                                    (course) => {
+                                                        
+                                                        if (!course) {
+                                                            return (
+                                                                <li
+                                                                    key={
+                                                                        course._id
+                                                                    }
+                                                                >
+                                                                    Unavailable
+                                                                </li>
+                                                            );
+                                                        }
+
+                                                        return (
+                                                            <ListItem
+                                                                key={course._id}
+                                                                item={course.name}
+                                                            />
+                                                        );
+                                                    },
+                                                )}
+                                            </ol>
+                                        ) : (
+                                            <p>No parent courses listed</p>
+                                        )}
+                                    </div>
+
+                                    <div className={styles.card}>
+                                        <h4>Prerequisite Courses</h4>
+                                        {course.prerequisites &&
+                                        course.prerequisites.length ? (
+                                            <ol className="chipList">
+                                                {course.prerequisites.map(
+                                                    (p) => {
+                                                        const crs = p.course;
+                                                        if (!crs) {
+                                                            return (
+                                                                <li
+                                                                    key={
+                                                                        p
+                                                                    }
+                                                                >
+                                                                    Unavailable
+                                                                </li>
+                                                            );
+                                                        }
+
+                                                        const display = `${crs.name} - Average Level Required: ${p.averageLevelRequired}`
+
+                                                        return (
+                                                            <ListItem
+                                                                key={p.course._id}
+                                                                item={display}
+                                                            />
+                                                        );
+                                                    },
+                                                )}
+                                            </ol>
+                                        ) : (
+                                            <p>
+                                                No prerequisite courses listed
+                                            </p>
+                                        )}
+                                    </div>
+                                </Card>
+                            )}
+
+                            {currentTab === 1 &&
+                                quizzes.map((quiz) => {
+                                    const userQuiz = userQuizzes.find(
+                                        (x) =>
+                                            x.quizId.toString() ===
+                                            quiz._id.toString(),
+                                    );
+
+                                    return (
+                                        <li key={quiz._id}>
+                                            <QuizDisplay quiz={quiz} />
+                                            {userQuiz && (
+                                                <UserStats
+                                                    userQuizInfo={userQuiz}
+                                                />
+                                            )}
+                                        </li>
+                                    );
+                                })}
+
+                            {currentTab === 2 &&
+                                notes.map((note) => {
+                                    const noteSources = note.sources.map(
+                                        (srcId) => {
+                                            const src = sources.find(
+                                                (source) =>
+                                                    source._id.toString() ===
+                                                    srcId.toString(),
+                                            );
+
+                                            if (!src) {
+                                                return {
+                                                    _id: srcId,
+                                                    title: "Unavailable",
+                                                    url: null,
+                                                };
+                                            }
+
+                                            return src;
+                                        },
+                                    );
+                                    return (
+                                        <li key={note._id}>
+                                            <Card
+                                                title={note.title}
+                                                description={`${note.text}`}
+                                            >
+                                                <div className={styles.card}>
+                                                    <h4>Sources linked</h4>
+                                                    <ul>
+                                                        {noteSources.map(
+                                                            (source) => {
+                                                                return (
+                                                                    <ListItem
+                                                                        key={
+                                                                            source._id
+                                                                        }
+                                                                        item={
+                                                                            source.title
+                                                                        }
+                                                                        link={
+                                                                            source.url
+                                                                        }
+                                                                    />
+                                                                );
+                                                            },
+                                                        )}
+                                                    </ul>
+                                                </div>
+
+                                                <div className={styles.card}>
+                                                    Created By: {note.createdBy}
+                                                </div>
+                                            </Card>
+                                        </li>
+                                    );
+                                })}
+
+                            {currentTab === 3 &&
+                                sources.map((source) => {
+                                    return (
+                                        <li key={source._id}>
+                                            <Card
+                                                title={source.title}
+                                                subtitle={source.medium}
+                                                buttons={[
+                                                    {
+                                                        label: "Visit the source page",
+                                                        link: source.url,
+                                                    },
+                                                ]}
+                                            >
+                                                <div className={styles.card}>
+                                                    <h4>Authors</h4>
+                                                    {source.authors &&
+                                                    source.authors.length ? (
+                                                        <ul className="chipList">
+                                                            {source.authors &&
+                                                                source.authors.map(
+                                                                    (cont) => (
+                                                                        <ListItem
+                                                                            key={
+                                                                                cont
+                                                                            }
+                                                                            item={
+                                                                                /^http/.test(
+                                                                                    cont,
+                                                                                )
+                                                                                    ? "See all of the authors"
+                                                                                    : cont
+                                                                            }
+                                                                            link={
+                                                                                /^http/.test(
+                                                                                    cont,
+                                                                                )
+                                                                                    ? cont
+                                                                                    : null
+                                                                            }
+                                                                        />
+                                                                    ),
+                                                                )}
+                                                        </ul>
+                                                    ) : (
+                                                        <p>No authors listed</p>
+                                                    )}
+                                                </div>
+                                            </Card>
+                                        </li>
+                                    );
+                                })}
+                        </ul>
+                    </main>
+                </div>
+            </section>
+        </main>
+    );
+}
--- app/courses/[id]/page.jsx
+++ app/courses/[id]/page.jsx
@@ -1,14 +1,8 @@
 import { redirect } from "next/navigation";
-import styles from "@/app/page.module.css";
-import { Course, Source, Note, Quiz } from "@/app/api/models";
+import { Course } from "@/app/api/models";
 import { cookies } from "next/headers";
-import { canRead, useUser, queryReadableResources } from "@/lib/auth";
-import {
-    NoteDisplay,
-    SourceDisplay,
-    CourseDisplay,
-} from "@/app/components/server";
-import { QuizDisplay, UserStats } from "@/app/components/client";
+import { canRead, useUser } from "@/lib/auth";
+import { CourseDash } from "./CourseDash";
 import { serializeOne } from "@/lib/db";
 
 export default async function CoursePage({ params }) {
@@ -24,129 +18,5 @@ export default async function CoursePage({ params }) {
         return redirect("/courses");
     }
 
-    const query = queryReadableResources(user);
-    const availableSources = await Source.find(query);
-    const availableNotes = await Note.find(query);
-    const availableQuizzes = await Quiz.find(query);
-    function filter(x) {
-        return x.courses.find((c) => c.toString() === id);
-    }
-    const sources = availableSources.filter(filter);
-    const notes = availableNotes.filter(filter);
-    const quizzes = availableQuizzes.filter(filter);
-
-    let sum = 0;
-    let countOfCanLevel = 0;
-    const userQuizzes = quizzes.map((q) => {
-        const userQuiz = user.quizzes.find(
-            (x) => x.quizId.toString() === q._id.toString(),
-        );
-        if (userQuiz) {
-            sum += userQuiz.level;
-            if(userQuiz.hiddenUntil < Date.now()){
-                countOfCanLevel++;
-            }
-            return userQuiz;
-        }
-        return {
-            quizId: q._id,
-            lastCorrect: new Date(0),
-            level: 0,
-            hiddenUntil: new Date(0),
-        };
-    });
-    const averageLevel = sum / quizzes.length;
-
-    return (
-        <main className={styles.main}>
-            <div className={styles.titleBlock}>
-                <h2>{course.name}</h2>
-                <p>{course.description}</p>
-                <p>Average Level: {averageLevel}</p>
-                <p>Quizzes Available to Level Up: {countOfCanLevel}</p>
-            </div>
-
-            {course.prerequisites.length > 0 && (
-                <section>
-                    <h3>Prerequisites</h3>
-                    <ul>
-                        {course.prerequisites.map((p) => {
-                            return (
-                                <li key={p.course.id}>
-                                    <p>{p.averageLevelRequired}</p>
-                                    <CourseDisplay course={p.course} />
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </section>
-            )}
-
-            {course.parentCourses.length > 0 && (
-                <section>
-                    <h3>Parent Courses</h3>
-                    <ul>
-                        {course.parentCourses.map((c) => {
-                            return (
-                                <li key={c.id}>
-                                    <CourseDisplay course={c} />
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </section>
-            )}
-
-            {sources.length > 0 && (
-                <section>
-                    <h3>Sources for {course.name}</h3>
-                    <ul>
-                        {sources.map((source) => (
-                            <li key={source._id}>
-                                <SourceDisplay source={source} />
-                            </li>
-                        ))}
-                    </ul>
-                </section>
-            )}
-
-            {notes.length > 0 && (
-                <section>
-                    <h3>Notes for {course.name}</h3>
-                    <ul>
-                        {notes.map((note) => (
-                            <li key={note._id}>
-                                <NoteDisplay note={note} />
-                            </li>
-                        ))}
-                    </ul>
-                </section>
-            )}
-
-            {quizzes.length > 0 && (
-                <section>
-                    <h3>Quizzes for {course.name}</h3>
-                    <ul>
-                        {quizzes.map((quiz) => {
-                            const userQuiz = userQuizzes.find(
-                                (x) =>
-                                    x.quizId.toString() === quiz._id.toString(),
-                            );
-                            return (
-                                <li key={quiz._id}>
-                                    <QuizDisplay
-                                        quiz={serializeOne(quiz)}
-                                        canClientCheck={false}
-                                    />
-                                    {userQuiz && (
-                                        <UserStats userQuizInfo={userQuiz} />
-                                    )}
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </section>
-            )}
-        </main>
-    );
+    return <CourseDash course={serializeOne(course)} />
 }
