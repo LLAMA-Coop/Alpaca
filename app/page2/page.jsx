f94bf5d87cc0ca9832c5a83bed32570b3a7242bb
Pragya <106767345+Kiztbz@users.noreply.github.com>
2024-01-18 21:16:37 +0530

    Update page.jsx
    Removing the double header

--- app/page2/page.jsx
+++ app/page2/page.jsx
@@ -19,24 +19,6 @@ function page() {
     return (
         <div className={styles.body}>
             <div className={styles.container}>
-                <nav className={styles.nav}>
-                    <a className={styles.Mneme} href="http://localhost:3001">
-                        Mneme
-                    </a>
-                    <ul className={styles.navigation}>
-                        <li>create</li>
-                        <li>learn</li>
-                        <li>test</li>
-                        <li>marketplace</li>
-                    </ul>
-                    <a
-                        href="http://localhost:3001/login"
-                        className={`${styles.loginBtn} ${styles.hover}`}
-                        style={{ color: "#0f172a" }}
-                    >
-                        login
-                    </a>
-                </nav>
                 <span className={styles.orb1}></span>
                 <span className={styles.orb2}></span>
                 {/* <!-- <span  className="star">
