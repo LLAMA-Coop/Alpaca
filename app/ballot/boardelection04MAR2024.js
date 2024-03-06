787b757b94bd4c4fa6f4d6960269ea40dde56b46
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2024-03-05 22:35:27 -0500

    Fix date on written waiver of notice

--- app/ballot/boardelection04MAR2024.js
+++ app/ballot/boardelection04MAR2024.js
@@ -3,7 +3,7 @@ const ballots = [
         heading: "Written Waiver of Notice",
         questions: [
             {
-                motion: "I acknowledge that I did not receive 10 days notice of this meeting. In accordance with our Operating Agreement in 4.4, I waive my notice and consent to attend the member meeting on March 5, 2024.",
+                motion: "I acknowledge that I did not receive 10 days notice of this meeting. In accordance with our Operating Agreement in 4.4, I waive my notice and consent to attend the member meeting on March 6, 2024.",
                 choices: ["Waive notice", "Do not waive notice"],
                 options: {
                     numberChoices: 1,
