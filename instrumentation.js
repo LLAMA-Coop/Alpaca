a7dd49f5161428133f27bd30ef80465d3fdb22d4
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2024-07-20 16:06:31 -0400

    Fix filename spelling instrumentation.js

--- instrumentation.js
+++ instrumentation.js
@@ -1,5 +1,5 @@
 export async function register() {
-    console.log("HOORAY! I'M REGISTERING!")
+    console.log("HOORAY! I'M REGISTERING!");
     if (process.env.NEXT_RUNTIME === "nodejs") {
         await import("./db-init.js");
     }
