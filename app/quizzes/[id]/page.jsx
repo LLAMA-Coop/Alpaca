d03fde52b6b88d46a1f47dc2d53934bff18a3f7d
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2024-08-11 21:37:11 -0400

    Continue switching for Quiz model

--- app/quizzes/[id]/page.jsx
+++ app/quizzes/[id]/page.jsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 import styles from "@/app/page.module.css";
-import { Quiz } from "@/app/api/models";
 import { cookies } from "next/headers";
 import {
     QuizDisplay,
@@ -10,29 +9,27 @@ import {
 } from "@/app/components/client";
 import { canRead, useUser } from "@/lib/auth";
 import { serializeOne } from "@/lib/db";
+import { getQuizzesById } from "@/lib/db/helpers";
 
 export default async function QuizPage({ params }) {
     const { id } = params;
 
-    const quiz = await Quiz.findById(id);
+    const quiz = (await getQuizzesById({ id }))[0];
 
     const user = await useUser({ token: cookies().get("token")?.value });
     if (!canRead(quiz, user)) {
         return redirect("/quizzes");
     }
 
-    await quiz.populate("createdBy");
-    await quiz.populate("contributors");
-
     return (
         <main className={styles.main}>
             <div className={styles.titleBlock}>
-                Created By: <UserCard user={serializeOne(quiz.createdBy)} />
+                Created By: <UserCard user={quiz.creator} />
                 <div>
                     <p>Contributors:</p>
                     <ul>
                         {quiz.contributors.map((c) => (
-                            <li key={c._id}>{c.username}</li>
+                            <li key={c.id}>{c.username}</li>
                         ))}
                     </ul>
                 </div>
