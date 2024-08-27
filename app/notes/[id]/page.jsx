309834935e7b4c0ab23262bfef786bd2dbec34b0
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2024-08-27 11:53:42 -0400

    Fix bug with /api/notes/[id]

--- app/notes/[id]/page.jsx
+++ app/notes/[id]/page.jsx
@@ -3,14 +3,14 @@ import styles from "@/app/page.module.css";
 import { cookies } from "next/headers";
 import { NoteDisplay } from "@/app/components/server";
 import { useUser } from "@/lib/auth";
-import { getPermittedNote } from "@/lib/db/helpers";
+import { getPermittedNotes } from "@/lib/db/helpers";
 import { NoteInput } from "@/app/components/client";
 
 export default async function NotePage({ params }) {
     const { id } = params;
 
     const user = await useUser({ token: cookies().get("token")?.value });
-    const note = await getPermittedNote(id, user.id);
+    const note = (await getPermittedNotes(user.id)).find((x) => x.id === id);
     if (!note) {
         return redirect("/notes");
     }
