dd3f31c4e0beff744eeb3f3de588ea8674288779
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2024-09-30 20:44:48 -0400

    Thanks to Mart1d4, fixed quizzes/[id]. It was actually mostly his code.

--- app/(mainapp)/quizzes/[id]/page.jsx
+++ app/(mainapp)/quizzes/[id]/page.jsx
@@ -1,42 +1,65 @@
 import styles from "@/app/(mainapp)/page.module.css";
 import { cookies } from "next/headers";
 import {
-    QuizDisplay,
-    UserCard,
-    QuizInput,
-    Card,
+  QuizDisplay,
+  UserCard,
+  QuizInput,
+  Card,
 } from "@/app/components/client";
+import {
+  jsonObject,
+  permissionsDefaultColumns,
+  userDefaultColumns,
+} from "@/lib/db/helpers";
+import { redirect } from "next/navigation";
 import { useUser } from "@/lib/auth";
-import { getPermittedResources } from "@/lib/db/helpers";
+import { db } from "@/lib/db/db";
 
 export default async function QuizPage({ params }) {
-    const { id } = params;
+  const { id } = params;
 
-    const user = await useUser({ token: cookies().get("token")?.value });
+  const user = await useUser({ token: cookies().get("token")?.value });
+  if (!user) redirect("/login");
 
-    const resources = await getPermittedResources({
-        withQuizzes: true,
-        userId: user?.id,
-        selectById: id
-    })
+  const quiz = await db
+    .selectFrom("quizzes")
+    .selectAll()
+    .select(({ selectFrom }) => [
+      selectFrom("resource_permissions as rp")
+        .select(jsonObject({ list: permissionsDefaultColumns, table: "rp" }))
+        .whereRef("rp.resourceId", "=", "quizzes.id")
+        .where("rp.resourceType", "=", "quiz")
+        .as("permissions"),
+      selectFrom("users")
+        .select(
+          jsonObject({
+            list: userDefaultColumns,
+            table: "users",
+          })
+        )
+        .whereRef("users.id", "=", "quizzes.createdBy")
+        .as("creator"),
+    ])
+    .where("id", "=", id)
+    .executeTakeFirst();
 
-    const quiz = resources.quizzes[0]
+  if (!quiz.permissions.allRead && !quiz.permissions.read.includes(user.id)) {
+    redirect("/quizzes");
+  }
 
-    // console.log("RESOURCES QUIZ", resources, quiz);
-
-    return (
-        <main className={styles.main}>
-            <div className={styles.titleBlock}>
-                Created By: <UserCard user={quiz.creator} />
-            </div>
-            <section>
-                <QuizDisplay quiz={quiz} />
-            </section>
-            <section>
-                <Card title={"Edit Quiz Question"}>
-                    <QuizInput quiz={quiz} />
-                </Card>
-            </section>
-        </main>
-    );
+  return (
+    <main className={styles.main}>
+      <div className={styles.titleBlock}>
+        Created By: <UserCard user={quiz.creator} />
+      </div>
+      <section>
+        <QuizDisplay quiz={quiz} />
+      </section>
+      <section>
+        <Card title={"Edit Quiz Question"}>
+          <QuizInput quiz={quiz} />
+        </Card>
+      </section>
+    </main>
+  );
 }
