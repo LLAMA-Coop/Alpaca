97ea7c98f7ed5610374a6508e43e93d77fb2e523
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2025-05-16 20:08:05 -0400

    began fixing getCourse problem

--- app/(mainapp)/[resource]/page.jsx
+++ app/(mainapp)/[resource]/page.jsx
@@ -85,7 +85,6 @@ export default async function ResourcePage(props) {
     });
 
     const resources = resourcesObject[current.name];
-    console.log(resources)
     if (!resources) return redirect("/");
 
     const hasMore = resources.length > amount;
--- app/(mainapp)/courses/[name]/page.jsx
+++ app/(mainapp)/courses/[name]/page.jsx
@@ -1,21 +1,35 @@
-import { getCourse } from "@/lib/db/helpers";
+import {
+  getCourse,
+  getPermittedResources,
+  getRelationships,
+} from "@/lib/db/helpers";
 import { redirect } from "next/navigation";
 import { CourseDash } from "./CourseDash";
 import { cookies } from "next/headers";
 import { useUser } from "@/lib/auth";
 
 export default async function CoursePage(props) {
-    const params = await props.params;
-    const user = await useUser({ token: (await cookies()).get("token")?.value });
-    const name = decodeURIComponent(params.name);
+  const params = await props.params;
+  const user = await useUser({ token: (await cookies()).get("token")?.value });
+  const name = decodeURIComponent(params.name);
 
-    const course = await getCourse({ name });
-    if (!course) return redirect("/courses");
+  // const course = await getCourse({ name });
+  const resources = await getPermittedResources({
+    userId: user.id,
+    takeAll: true,
+  });
 
-    return (
-        <CourseDash
-            course={course}
-            isLogged={!!user}
-        />
-    );
+  console.log("RESOURCES", resources);
+
+  const course = resources.courses.find((x) => x.name === name);
+
+  const relationships = await getRelationships({
+    type: "course",
+    id: course.id,
+  });
+  console.log(course);
+
+  if (!course) return redirect("/courses");
+
+  return <CourseDash course={course} isLogged={!!user} />;
 }
